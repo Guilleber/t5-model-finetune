6dc6ce2cd3d9603fc7a755047967e1bdf9e69e2f
Guillaume Le Berre <guillaume.le_berre@depinfonancy.net>
2021-12-24 15:33:29 +0100

    save changes

--- gridrun_bis.sh
+++ gridrun_bis.sh
@@ -1,6 +1,6 @@
 #python3 t5_train.py --model-name t5-large --datasets openbookqa --epochs 4 --gpus 1 --save-model ./stored_models/baseline_t5-large_obqa.ckpt
 #python3 t5_train.py --model-name unifiedqa-large-lr1 --datasets synthetic_wiki_bis --epochs 2 --gpus 1 --save-model ./stored_models/unifiedqa-large_synthetic_wiki_bis_2e.ckpt
-/usr/bin/python3 train.py --model_name unifiedqa-large-lr1 --datasets enhanced_synthetic_wiki_1 --epochs 4 --gpus 1 --save_best_model --exp_name unifiedqa-large_curriculum --load_model_from ./saved_models/unifiedqa-large_synthetic_wiki-v2.ckpt 
+/usr/bin/python3 train.py --model_name unifiedqa-large-lr1 --datasets "enhanced_synthetic_wiki_1 sciq" --epochs 4 --gpus 1 --save_best_model --exp_name unifiedqa-large_dataaugment_sciq
 #python3 t5_train.py --model-name unifiedqa-large-lr1 --datasets enhanced_synthetic_wiki --epochs 4 --gpus 1 --save-model ./stored_models/unifiedqa-large_enhanced_synthetic_wiki.ckpt
 #python3 t5_train.py --model-name unifiedqa-large-lr1 --datasets synthetic_sciq --epochs 1 --gpus 1 --save-model ./stored_models/unifiedqa-large_synthetic_sciq.ckpt
 #python3 t5_train.py --model-name unifiedqa-large-lr1 --datasets synthetic_wiki --epochs 1 --gpus 1 --save-model ./stored_models/unifiedqa-large_synthetic_wiki.ckpt
--- t5_eval_scripts/gridrun_bis.sh
+++ t5_eval_scripts/gridrun_bis.sh
@@ -1,2 +1,2 @@
 #./t5_eval.sh -f -d qasc -t unifiedqa-large -- ../../../unifiedqa_datasets/qasc/dev.jsonl
-./t5_eval.sh -f -d sciq -t unifiedqa-large -m unifiedqa-large_synthetic_wiki-v2 -- ../../../unifiedqa_datasets/sciq/dev.jsonl
+./t5_eval.sh -f -d qasc -t unifiedqa-large -m unifiedqa-large_synthetic_wiki-v2 -- ../../../unifiedqa_datasets/qasc/test.jsonl
